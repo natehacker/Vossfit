6a32ecc857984fcd200e95827950f44dd8160206
Nathan louissaint <louissaintmark@gmail.com>
2022-12-30 20:48:04 -0500

    nathan

--- my-app/src/Possibility.jsx
+++ my-app/src/Possibility.jsx
@@ -5,7 +5,7 @@ import './Possibility.css';
 const Possibility = () => (
   <div className="gpt3__possibility section__padding" id="possibility">
     <div className="gpt3__possibility-image">
-      {/* <img src={possibilityImage} alt="possibility" /> */}
+    <img src="" alt=""/>
     </div>
     <div className="gpt3__possibility-content">
       
--- my-app/src/components/containers/Blog.jsx
+++ my-app/src/components/containers/Blog.jsx
@@ -10,27 +10,12 @@ const Blog = () => (
 <h1 className="gradient__text"> WHAT IS<br />VOSS-FITNESS.</h1>
       <Container>
       <div class="ratio ratio-16x9">
-  <iframe src="https://www.youtube.com/embed/Yoi_zB4OYp4" title="YouTube video" allowfullscreen></iframe>
+  <iframe src="https://www.youtube.com/embed/LCu22mhDCD0" title="YouTube video" allowfullscreen></iframe>
 </div>
       </Container>
-
-
-      
-
-
     </div>
-    <div className="gpt3__blog-container">
-      <div className="gpt3__blog-container_groupA">
-        {/* { <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" /> } */}
-      </div>
-      <div className="gpt3__blog-container_groupB">
-        {/* <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" /> */}
-      </div>
     </div>
-  </div>
+
 );
 
 export default Blog;
