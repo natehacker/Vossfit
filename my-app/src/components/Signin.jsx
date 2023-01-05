1d004b845f0dc678bd6e8269c8f1746bf24d2b43
Nathan louissaint <louissaintmark@gmail.com>
2023-01-05 09:55:53 -0500

    sign in logo

--- my-app/src/components/Signin.jsx
+++ my-app/src/components/Signin.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import "./signin.css"
+import vossrun from './assests/vossrun.jpg';
+
 const Signin = () => {
   return (
-    <div>Signin componenet</div>
+
+    <div className='gpt-center'>
+    <div class="text-center">
+
+<div class="container-xl" id="gpt-nathan">
+ <h3 id="gpt">Welcome Back!</h3>
+ <input type="text" id="inputp" placeholder="UserName"/>
+ <input type="text" id="inputp" placeholder="Password"/>
+ <button class="n"id="gptn"><span>Login</span></button>
+ <p id="sucesso"></p>
+</div>
+
+<div class="container-xl" id="gpt-louissaint">
+ <img class= "gpt-m"src={vossrun} alt="run"/>
+</div>
+
+</div>
+
+ </div>
+ 
   )
 }
 
